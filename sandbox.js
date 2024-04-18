b7f2ebba0a642cc4c763a8e60391ac2cc34111e5
Negar <negarsaljooghi@gmail.com>
2024-04-18 10:07:46 +0330

    remove sth from webpage

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,11 @@
+//const ul = document.querySelector('ul');
+//ul.remove();
+
 const items = document.querySelectorAll('li');
 
 items.forEach(item => {
     item.addEventListener('click', e => {
-        e.target.style.textDecoration = 'line-through';
+        //e.target.style.textDecoration = 'line-through';
+        e.target.remove();
     });
 });
